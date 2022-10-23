c72d4e8664acae6d7cbb0f555d787f51d1f06c54
andrinisme <nugrahandrik@gmail.com>
2022-10-23 15:55:41 +0800

    last change

--- module4-solution/SpeakGoodbye.js
+++ module4-solution/SpeakGoodbye.js
@@ -5,7 +5,7 @@
 // See Lecture 52, part 2
 (function (window) {
 
-// STEP 7: Create an object, called 'byeSpeaker' to which you will attach
+// STEP 7: Create an object , called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
 var byeSpeaker = {};
